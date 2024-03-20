2cd134ecec4418e6fef226ae28a5368cbbc4ca0d
ANUPRIYA <anupriyapradeepanus@gmail.com>
2024-03-20 10:27:08 +0530

    commit

--- lib/alert_dialogue box.dart
+++ lib/alert_dialogue box.dart
@@ -1,45 +1,103 @@
 import 'package:flutter/material.dart';
-void main()
-{
+
+void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: alert(),
+    home: FirstPage(),
   ));
 }
-class alert extends StatefulWidget
-{
-  @override
-  State<alert> createState() => AlertState();
-}
-class AlertState extends State<alert>
-{
+
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     body:Center(
-       child: ElevatedButton(//style:ElevatedButton.styleFrom(backgroundColor:Color(colors.))),
-         onPressed: (){
-           showDialog(context: context, builder: (context){
-             return AlertDialog(
-               title: Text("Exit"),
-               content: Text("Do you want to exit !"),
-               actions: [
-                 TextButton(onPressed: (){
-                   Navigator.of(context).pop();   // click chythal alert box cancel aavan
-                 }, child: Text("Yes"),),
-                 TextButton(onPressed: (){
-                   Navigator.of(context).pop();
-                 }, child: Text("No")),
-                 TextButton(onPressed: (){
-                   Navigator.of(context).pop();
-                 }, child: Text("Cancel")),
-               ],
-             );
-           });
-         }, child: Text("Alert",style: TextStyle(color: Colors.black),),
-       ),
-     )
-   );
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => Alert(),
+            );
+          },
+          child: Text("Alert", style: TextStyle(color: Colors.black)),
+        ),
+      ),
+    );
   }
-  
 }
+
+class Alert extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Exit"),
+      content: Text("Do you want to exit!"),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Clicking 'Yes' closes the alert box
+          },
+          child: Text("Yes"),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Clicking 'No' closes the alert box
+          },
+          child: Text("No"),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Clicking 'Cancel' closes the alert box
+          },
+          child: Text("Cancel"),
+        ),
+      ],
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+// void main()
+// {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: alert(),
+//   ));
+// }
+// class alert extends StatefulWidget
+// {
+//   @override
+//   State<alert> createState() => AlertState();
+// }
+// class AlertState extends State<alert>
+// {
+//   @override
+//   Widget build(BuildContext context) {
+//    return Scaffold(
+//      body:Center(
+//        child: ElevatedButton(//style:ElevatedButton.styleFrom(backgroundColor:Color(colors.))),
+//          onPressed: (){
+//            showDialog(context: context, builder: (context){
+//              return AlertDialog(
+//                title: Text("Exit"),
+//                content: Text("Do you want to exit !"),
+//                actions: [
+//                  TextButton(onPressed: (){
+//                    Navigator.of(context).pop();   // click chythal alert box cancel aavan
+//                  }, child: Text("Yes"),),
+//                  TextButton(onPressed: (){
+//                    Navigator.of(context).pop();
+//                  }, child: Text("No")),
+//                  TextButton(onPressed: (){
+//                    Navigator.of(context).pop();
+//                  }, child: Text("Cancel")),
+//                ],
+//              );
+//            });
+//          }, child: Text("Alert",style: TextStyle(color: Colors.black),),
+//        ),
+//      )
+//    );
+//   }
+//
+// }
