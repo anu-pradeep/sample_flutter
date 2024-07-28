737d6de4a8496012e08ba03d13ec1bd587204b53
ANUPRIYA <anupriyapradeepanus@gmail.com>
2024-07-28 18:48:51 +0530

    commit

--- lib/shared_preference/login page.dart
+++ lib/shared_preference/login page.dart
@@ -3,17 +3,20 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'home page.dart';
-void main()
-{
-  runApp(MaterialApp(home: sharelogin(),debugShowCheckedModeBanner: false,));
+
+void main() {
+  runApp(MaterialApp(
+    home: sharelogin(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
-class sharelogin extends StatefulWidget
-{
+
+class sharelogin extends StatefulWidget {
   @override
-  State<sharelogin> createState()=> sharestate();
+  State<sharelogin> createState() => sharestate();
 }
-class sharestate extends State<sharelogin>
-{
+
+class sharestate extends State<sharelogin> {
   late SharedPreferences data;
   TextEditingController username = TextEditingController();
   TextEditingController password = TextEditingController();
@@ -23,81 +26,102 @@ class sharestate extends State<sharelogin>
     checkuser();
     super.initState();
   }
-  Future<void> checkuser()
-  async {
-      data =await SharedPreferences.getInstance();
-      newuser = (data.getBool("newuser") ?? true);
-      if( newuser == false)
-        {
-          Navigator.push(context,MaterialPageRoute(builder: (context)=> homie()));
+
+  Future<void> checkuser() async {
+    data = await SharedPreferences.getInstance();
+    newuser = (data.getBool("newuser") ?? true);
+    if (newuser == false) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => homie()));
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(title: Text("Shared preferences",style: GoogleFonts.aBeeZee(fontWeight: FontWeight.bold,fontSize: 20),),
-      centerTitle: true,),
+      appBar: AppBar(
+        title: Text(
+          "Shared preferences",
+          style: GoogleFonts.aBeeZee(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        centerTitle: true,
+      ),
       body: Container(
-        child:Center(
-        child: Column(
-          children: [Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Image(image:NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdjLlJS2C2KD-fRoOykz8e5luqOtFFpGo_QQ&usqp=CAU"),),
-          ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10,right: 10,top: 80),
-              child: TextFormField(controller: username,
-                decoration: InputDecoration(
-                  hintText: "user name", labelText: "user name",
-                  prefixIcon: Icon(Icons.mail_outline_rounded),
+        child: Center(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Image(
+                  image: NetworkImage(
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdjLlJS2C2KD-fRoOykz8e5luqOtFFpGo_QQ&usqp=CAU"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10, top: 80),
+                child: TextFormField(
+                  controller: username,
+                  decoration: InputDecoration(
+                    hintText: "user name",
+                    labelText: "user name",
+                    prefixIcon: const Icon(Icons.mail_outline_rounded),
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(25.0),
-                      borderSide:  BorderSide(color: Colors.pinkAccent),
+                      borderSide: const BorderSide(color: Colors.pinkAccent),
                     ),
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(25.0),
-                      borderSide:  BorderSide(color: Colors.pinkAccent),
+                      borderSide: const BorderSide(color: Colors.pinkAccent),
                     ),
-
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10,right: 10,top: 20),
-              child: TextFormField(
-                controller: password,
-                decoration: InputDecoration(
-                  hintText: "password",labelText: "password",
-                  prefixIcon: Icon(Icons.lock_open),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                    borderSide:  BorderSide(color: Colors.pinkAccent),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                    borderSide:  BorderSide(color: Colors.pinkAccent),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: ElevatedButton(onPressed: (){
-                String uname =username.text;
-                String pwd =password.text;
-                if(uname != '' && pwd != ''){print("login success");}
-                data.setString("username", uname);
-                data.setBool("newuser", false);
-                print(uname);
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> homie()));
-              },
-                  child: Text("Login",style: GoogleFonts.aBeeZee(fontSize: 15,),
-                  ),style: ElevatedButton.styleFrom(backgroundColor: Colors.red)),),
-          ],
-        ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+                child: TextFormField(
+                  controller: password,
+                  decoration: InputDecoration(
+                    hintText: "password",
+                    labelText: "password",
+                    prefixIcon: const Icon(Icons.lock_open),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: const BorderSide(color: Colors.pinkAccent),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: const BorderSide(color: Colors.pinkAccent),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: ElevatedButton(
+                    onPressed: () {
+                      String uname = username.text;
+                      String pwd = password.text;
+                      if (uname != '' && pwd != '') {
+                        print("login success");
+                      }
+                      data.setString("username", uname);
+                      data.setBool("newuser", false);
+                      print(uname);
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => homie()));
+                    },
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                    child: Text(
+                      "Login",
+                      style: GoogleFonts.aBeeZee(
+                        fontSize: 15,
+                      ),
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-
 }
